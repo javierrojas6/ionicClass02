4d86406d39c24c77f4ddf1c8ec96c1c4078614bd
Javier Rojas <javierrojas6@gmail.com>
2018-07-16 23:11:47 -0500

    prueba de concepto login en front funcionando

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
+import { Usuario } from './../../models/usuario';
 import { Proxy } from './../../helpers/proxy/proxy';
-import { Component, Injectable } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { Product } from '../../models/product';
@@ -10,13 +11,60 @@ import { Product } from '../../models/product';
 })
 
 @Injectable()
-export class HomePage {
+export class HomePage implements OnInit {
   productos: Product;
+  user: Usuario;
+  isLogged = false;
   constructor(public navCtrl: NavController, private proxy: Proxy) {
+    /* this.proxy.consult('http://localhost:3000/product/all', {}, 'get')
+       .subscribe(result => {
+         console.log(result);
+       });*/
+  }
+  ngOnInit(): void {
+    var a = localStorage.getItem('user');
+    if (a) {
+      this.isLogged = true;
+      a = JSON.parse(a);
+      this.user = new Usuario();
+      this.user.nombre = a.firstName;
+      this.user.email = a.email;
+    }
+  }
+  login(event) {
+    let params = {
+      email: event.target[0].value,
+      password: event.target[1].value,
+    };
+    console.log(params);
+    this.proxy.consult('http://localhost:3000/user/login', params, 'post')
+      .subscribe(
+        result => {
+          console.log(result);
+          if (result.status) {
+            //logn exitoso
+            localStorage.setItem('user', JSON.stringify(result.content));
+            localStorage.setItem('token', result.content.token);
 
-    this.proxy.consult('http://localhost:3000/product/all', {}, 'get')
-      .subscribe(result => {
-        console.log(result);
-      });
+            this.user = new Usuario();
+            this.user.nombre = result.content.firstName;
+            this.user.email = result.content.email;
+
+            this.isLogged = true;
+          } else {
+            console.log(result.content)
+          }
+        },
+        error => {
+          if (error.status == 401) {
+            console.log(error.statusText);
+          }
+        }
+      );
+  }
+  logOut() {
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+    this.isLogged = false;
   }
 }
